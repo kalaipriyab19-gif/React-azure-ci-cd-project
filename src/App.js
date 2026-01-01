f0e4c9a0c413a2c628623e615785b527c34a1cf7
Naveen2k22 <naveen2001010601@gmail.com>
2026-01-01 12:22:20 +0530

    Local commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Blue-Green Deployment</h1>
+      <h2>React App on Azure App Service</h2>
+      <p>Deployed using Azure DevOps CI/CD Pipeline</p>
+    </div>
+  );
+}
+
+export default App;
